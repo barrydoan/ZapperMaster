a1ddcf852166d81f605642addf54c4b998ad448e
Nhat Doan <74613870+barrydoan@users.noreply.github.com>
2023-04-06 09:37:54 -0400

    add edit project

---
+++ phone/EditorFragment/app/src/main/java/edu/temple/editorfragment/ButtonDTO.kt
@@ -0,0 +1,10 @@
+package edu.temple.editorfragment
+
+data class ButtonDTO(
+    val backgroundColor: String,
+    val size: String,
+    val topPositionPercent: Double,
+    val leftPositionPercent: Double,
+    val displayName: String,
+    val code: String
+)
---
+++ phone/EditorFragment/app/src/main/java/edu/temple/editorfragment/MainActivity.kt
@@ -0,0 +1,15 @@
+package edu.temple.editorfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView,EditorFragment())
+            .commit()
+    }
+}
